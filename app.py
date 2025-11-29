eb64d47e897cd0143c2619466b4572e33ca027d3
Felipe-HCR <Felipe-HCR@users.noreply.github.com.>
2025-11-28 22:26:00 -0600

    prueba con casillas de verificación

--- app.py
+++ app.py
@@ -8,22 +8,50 @@ st.header("Análisis Exploratorio de Datos de Vehículos en EE.UU.")
 # Leer los datos del archivo CSV
 car_data = pd.read_csv('vehicles_us.csv')
 
+# crear una casilla de verificación de histograma
+build_histogram = st.checkbox('Construir un histograma')
+# crear una casilla de verificación de dispersión
+build_scatter = st.checkbox('Construir un diagrama de dispersión')
+
 # Crear un botón en la aplicación Streamlit
-hist_button = st.button('Construir histograma')
+show_diagram_button = st.button('Construir diagrama selecionado')
 
 # Lógica a ejecutar cuando se hace clic en el botón
-if hist_button:
-    # Escribir un mensaje en la aplicación
-    st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+if show_diagram_button:
+    if build_histogram:  # si la casilla de verificación histograma está seleccionada
+        # Escribir un mensaje en la aplicación
+        st.write(
+            'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
-    # Crear un histograma utilizando plotly.graph_objects
-    # Se crea una figura vacía y luego se añade un rastro de histograma
-    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+        # Crear un histograma utilizando plotly.graph_objects
+        # Se crea una figura vacía y luego se añade un rastro de histograma
+        fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
 
-    # Agregamos un título al gráfico
-    fig.update_layout(title_text='Distribución del Odómetro')
+        # Agregamos un título al gráfico
+        fig.update_layout(title_text='Distribución del Odómetro')
 
-    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
-    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
-    st.plotly_chart(fig, use_container_width=True)
+        # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+        # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+        st.plotly_chart(fig, use_container_width=True)
+
+    elif build_scatter:  # si la casilla de verificación dispersión está seleccionada
+        # Escribir un mensaje en la aplicación
+        st.write(
+            'Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+        # Crear un diagrama de dispersión utilizando plotly.graph_objects
+        fig = go.Figure(data=go.Scatter(
+            x=car_data['odometer'],
+            y=car_data['price'],
+            mode='markers'  # Modo de marcador para puntos individuales
+        ))
+
+        # Agregamos un título al gráfico y etiquetas a los ejes
+        fig.update_layout(
+            title_text='Diagrama de Dispersión del Precio vs Año',
+            xaxis_title='Año',
+            yaxis_title='Precio'
+        )
+
+        # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+        st.plotly_chart(fig, use_container_width=True)
